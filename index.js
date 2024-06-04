9d924849c0920842a299ee8bf394c10c483cb176
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-04 15:57:38 -0600

    f class funct lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(names) {
+    return names.slice(0,2);
+}
+const returnLastTwoDrivers = function(names) {
+    return names.slice(names.length - 2,names.length)
+    
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+
+    return function(fare) { 
+        return fare*multiplier
+
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(names,returnFunc) {
+    return returnFunc(names)
+}
+console.log (selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'],returnFirstTwoDrivers))
+
